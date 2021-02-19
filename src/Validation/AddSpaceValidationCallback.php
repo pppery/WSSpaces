7eac781b3fbe1cce32f67a6485e45d520eaef83c
Marijn van Wezel <marijnvanwezel@gmail.com>
2021-02-19 13:37:14 +0100

    Fix issue where SMW properties would not be available

--- src/Validation/AddSpaceValidationCallback.php
+++ src/Validation/AddSpaceValidationCallback.php
@@ -71,9 +71,11 @@ class AddSpaceValidationCallback extends AbstractValidationCallback {
      * @return bool|string
      */
     private function validateNamespaceName( $value, array $form_data ) {
-        $valid = ctype_alnum( str_replace( ' ', '', $value ) );
+        if ( $value === null ) {
+            return true;
+        }
 
-        if ( !$valid ) {
+        if ( !$this->isValidNamespaceName( $value ) ) {
             return wfMessage( "wss-invalid-namespace-name" )->parse();
         }
 
@@ -94,5 +96,33 @@ class AddSpaceValidationCallback extends AbstractValidationCallback {
         }
 
         return wfMessage( "wss-namespace-name-in-use" )->parse();
+    }
+
+    /**
+     * Checks if the given namespace name (hub key) is valid.
+     *
+     * @param string $value
+     * @return bool
+     */
+    private function isValidNamespaceName( string $value ): bool {
+        $is_alphanumeric = ctype_alnum( str_replace( ' ', '', $value ) );
+
+        if ( !$is_alphanumeric ) {
+            return false;
+        }
+
+        $is_sufficient_length = strlen( $value ) > 3;
+
+        if ( !$is_sufficient_length ) {
+            return false;
+        }
+
+        $starts_with_letter = ctype_alpha( $value[0] );
+
+        if ( !$starts_with_letter ) {
+            return false;
+        }
+
+        return true;
     }
 }
