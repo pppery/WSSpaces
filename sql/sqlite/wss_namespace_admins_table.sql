19e73e9604e12b35e535d9fb54a390c40a193fdd
Wout Gevaert <wout@wikibase.nl>
2022-11-21 12:30:11 +0100

    Add wgDBPrefix to references in sql statements
    See Wikimedia\Rdbms\Database::replaceVars

--- sql/sqlite/wss_namespace_admins_table.sql
+++ sql/sqlite/wss_namespace_admins_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE /*_*/wss_namespace_admins (
       admin_user_id INT NOT NULL,
       PRIMARY KEY ( namespace_id, admin_user_id ),
       FOREIGN KEY ( namespace_id )
-          REFERENCES wss_namespaces( namespace_id )
+          REFERENCES /*_*/wss_namespaces( namespace_id )
           ON DELETE CASCADE
 ) /*$wgDBTableOptions*/;
